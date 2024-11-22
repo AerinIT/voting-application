dbf407d6002066624e1f8793abb23f7618a60797
manish-arin <manish@arintech.in>
2024-11-22 07:20:59 +0000

    working

--- voting-app-server/tracing.js
+++ voting-app-server/tracing.js
@@ -4,9 +4,13 @@ const {
 } = require('@opentelemetry/auto-instrumentations-node');
 const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');``
 const sdk = new opentelemetry.NodeSDK({
   metricReader: new PrometheusExporter({
-    endpoint: '/metrics'
+    endpoint: '/metrics',
+  }),
+  tempoExporter: new OTLPTraceExporter({
+    url: 'http://alloy:4318/v1/traces',
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
